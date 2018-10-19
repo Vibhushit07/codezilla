25cea376933f05310746d2574316fe68490cca4b
Vibhushit Singhal <41487076+Vibhushit07@users.noreply.github.com>
2018-10-19 18:17:07 +0530

    Update factorial.c

--- Maths/Factorial/C/factorial.c
+++ Maths/Factorial/C/factorial.c
@@ -1,11 +1,13 @@
 #include "stdio.h"
-
+/* factorial function will find the factorial of 5.
+   Recursion is used here.
+*/
 long long factorial(long long n) {
 	if(n == 1) {
 		return 1;
 	}
 	else {
-		return n * factorial(n-1);
+		return n * factorial(n-1); 
 	}
 }
 
